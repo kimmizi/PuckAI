c8659707f3d4dcce8ccbe3b6a658199c1a26e43b
Kim Zierahn <kim@kappa-mm.de>
2025-02-19 09:24:37 +0100

    PPO optimization

--- exp/PPG_batch.py
+++ exp/PPG_batch.py
@@ -6,33 +6,31 @@ from hockey.hockey_env import Mode
 from KI_PPO_ppg2 import PPO_2, Memory
 
 
-env = h_env.HockeyEnv_BasicOpponent(mode = Mode.NORMAL, weak_opponent = False)
+env = h_env.HockeyEnv_BasicOpponent(mode = Mode.TRAIN_SHOOTING, weak_opponent = False)
 # env = h_env.HockeyEnv(mode = Mode.NORMAL)
 state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.shape[0]
 max_action = float(env.action_space.high[0])
 
 checkpoint_freq = 500
-checkpoint_dir = "./checkpoints_ppg"
+checkpoint_dir = "./checkpoints_ppg_shoot"
 
-update_timestep = 1000
-
+update_timestep = 149
 
 ppo_2 = PPO_2(
     state_dim,
     action_dim,
-    n_latent_var = 149,
-    lr = 0.00042287386768569786,
+    n_latent_var = 43,
+    lr = 8.988645063280326e-05,
     betas = (0.9, 0.999),
-    gamma = 0.9823205435136207,
-    K_epochs = 10,
-    eps_clip = 0.20809975884665038,
+    gamma = 0.9967844742150109,
+    K_epochs = 24,
+    eps_clip = 0.2788302938997803,
     has_continuous_action_space = True,
-    action_std_init = 0.8101787524225987
+    action_std_init = 0.3462835582997581
 )
 
-
-def train_ppg(env, ppo_agent, num_episodes, aux_phase_freq=5):
+def train_ppg(env, ppo_agent, num_episodes, aux_phase_freq):
     """
     Training loop with PPG.
     """
@@ -49,15 +47,29 @@ def train_ppg(env, ppo_agent, num_episodes, aux_phase_freq=5):
         # Policy phase: Collect trajectories
         while not done:
             action = ppo_agent.policy_old.act(state, memory)
+
             next_state, reward, done, _, info = env.step(action)
             next_state = next_state.flatten()
+
             memory.rewards.append(reward)
             memory.is_terminals.append(done)
+
             episode_reward += reward  # Accumulate reward
             state = next_state
 
-        # Update policy using PPO
-        # ppo_agent.update(memory)
+        # while not done:
+        #     action = ppo_agent.policy_old.act(state, memory)
+        #
+        #     opponent_action = np.zeros(action_dim)
+        #     final_action = np.concatenate([action, opponent_action])
+        #
+        #     next_state, reward, done, t, info = env.step(final_action)
+        #     next_state = state.flatten()
+        #
+        #     memory.rewards.append(reward)
+        #     memory.is_terminals.append(done)
+        #     episode_reward += reward
+        #     state = next_state
 
         # Auxiliary phase: Train value function
         if episode % aux_phase_freq == 0:
@@ -68,8 +80,6 @@ def train_ppg(env, ppo_agent, num_episodes, aux_phase_freq=5):
             memory.clear_memory()
             timestep = 0
 
-        # memory.clear_memory()
-
         # Log episode results
         episode_rewards.append(episode_reward)
         info_list.append(info.get("winner", None))
@@ -78,12 +88,12 @@ def train_ppg(env, ppo_agent, num_episodes, aux_phase_freq=5):
         if episode % checkpoint_freq == 0:
             ppo_agent.save_checkpoint(checkpoint_dir, episode)
             # Save results
-            np.save("episode_rewards_ppg2.npy", episode_rewards_ppg2)
-            np.save("info_list_ppg2.npy", info_list_ppg2)
+            np.save("episode_rewards_ppg2.npy", episode_rewards)
+            np.save("info_list_ppg2.npy", info_list)
 
-    return episode_rewards, info_list
+    return episode_rewards, info_list, ppo_agent
 
-episode_rewards_ppg2, info_list_ppg2 = train_ppg(env, ppo_2, num_episodes=100001, aux_phase_freq=20)
+episode_rewards_ppg2, info_list_ppg2, ppo_2 = train_ppg(env, ppo_2, num_episodes = 100001, aux_phase_freq = 72)
 
 # Save final model
 ppo_2.save_checkpoint(checkpoint_dir, "final")
