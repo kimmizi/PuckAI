b7e3ca87274c90d64a8ed3b728ad69dc428fa1ff
Kim Zierahn <kim@kappa-mm.de>
2025-02-23 22:03:45 +0100

    PPG

--- exp/PPG_batch.py
+++ exp/PPG_batch.py
@@ -5,73 +5,65 @@ import numpy as np
 import random
 import torch
 import matplotlib.pyplot as plt
-from PPG_improved import PPO_init, Memory
 
 
-env = h_env.HockeyEnv_BasicOpponent(mode = Mode.NORMAL, weak_opponent = False)
-
-# env = h_env.HockeyEnv(mode = Mode.NORMAL)
-state_dim = env.observation_space.shape[0]
-action_dim = env.action_space.shape[0]
-max_action = float(env.action_space.high[0])
+env_hockey = h_env.HockeyEnv_BasicOpponent(mode = Mode.NORMAL, weak_opponent = False)
+state_dim = env_hockey.observation_space.shape[0]
+action_dim = env_hockey.action_space.shape[0]
 
 
-ppo_2 = PPO_init(
+from PPG_GAE_KL import PPO, Memory
+
+ppg_KL = PPO(
     state_dim,
     action_dim,
     n_latent_var_actor = 128,
-    n_latent_var_critic = 256,
-    lr = 0.00003,
-    betas = (0.9, 0.999),
-    gamma = 0.95,
-    K_epochs = 10,
-    eps_clip = 0.25,
+    n_latent_var_critic = 128,
+    network_depth_actor = 2,
+    network_depth_critic = 2,
     has_continuous_action_space = True,
     action_std_init = 0.5,
+    lr = 0.0003,
+    betas = (0.9, 0.999),
+    gamma = 0.99,
+    K_epochs = 10,
+    eps_clip = 0.25,
     c1 = 0.5,
     c2 = 0.01,
-    beta_clone = 0.95,
-    network_depth_actor = 2,
-    network_depth_critic = 4
+    beta_clone = 0.95
 )
 
+episodes = 5000
+ppg_KL.load_checkpoint("checkpoints_ppg_kl/checkpoint_final.pth")
+print("Model loaded")
 
-checkpoint_freq = 10000
-checkpoint_dir = "./checkpoints_ppg_init/"
+checkpoint_freq = 1000
+checkpoint_dir = "./checkpoints_ppg_kl"
 
 update_timestep = 500
 
-episodes = 100001
-checkp = "checkpoint_final.pth"
-ppo_2.load_checkpoint(checkpoint_dir + checkp)
 
-checkpoint_dir = "./checkpoints_ppg_init"
-
-def train_ppg(env, ppo_agent, num_episodes, aux_phase_freq):
-    """
-    Training loop with PPG.
-    """
+def train_hockey_ppg(env, max_episodes, max_timesteps, update_timestep, aux_phase_freq, agent):
     memory = Memory()
-    episode_rewards = []  # Track rewards for each episode
-    info_list = []  # Track additional info (e.g., winner)
+    episode_rewards = []
+    info_list = []
 
-    for episode in range(episodes, num_episodes):
-        state, _ = env.reset()
-        state = state.flatten()
+    for episode in range(episodes, max_episodes):
+        # state, _ = env.reset()
+        # state = state.flatten()
         done = False
         episode_reward = 0
 
+        # Randomly choose weak or strong opponent
         env = h_env.HockeyEnv(mode = Mode.NORMAL)
-        o, info = env.reset()
-
+        state, _ = env.reset()
+        state = state.flatten()
         mode_random = random.choice([True, False])
-
         player2 = h_env.BasicOpponent(weak = mode_random)
         obs_agent2 = env.obs_agent_two()
 
-        # Policy phase: Collect trajectories
         while not done:
-            action = ppo_agent.policy_old.act(state, memory)
+            action = agent.policy_old.act(state, memory)
 
             action_opp = player2.act(obs_agent2)
 
@@ -81,35 +73,44 @@ def train_ppg(env, ppo_agent, num_episodes, aux_phase_freq):
             memory.rewards.append(reward)
             memory.is_terminals.append(done)
 
-            episode_reward += reward  # Accumulate reward
+            episode_reward += reward
             state = next_state
 
             obs_agent2 = env.obs_agent_two()
 
+            if done:
+                break
 
         # Auxiliary phase: Train value function
         if episode % aux_phase_freq == 0:
-            ppo_agent.auxiliary_phase(memory)
+            agent.auxiliary_phase(memory)
+            print(f"Episode {episode + 1}, Reward: {episode_reward}")
 
         if episode % update_timestep == 0:
-            ppo_agent.update(memory)
+            agent.update(memory)
             memory.clear_memory()
-
-        # ppo_agent.update(memory)
-        # memory.clear_memory()
 
         # Log episode results
         episode_rewards.append(episode_reward)
         info_list.append(info.get("winner", None))
-        print(f"Episode {episode + 1}, Reward: {episode_reward}")
 
         if episode % checkpoint_freq == 0:
-            ppo_agent.save_checkpoint(checkpoint_dir, episode)
+            agent.save_checkpoint(checkpoint_dir, episode)
 
-    return episode_rewards, info_list, ppo_agent
+    return episode_rewards, info_list, agent
 
-episode_rewards_ppg2, info_list_ppg2, ppo_2 = train_ppg(env, ppo_2, num_episodes = 200000, aux_phase_freq = 50)
+
+# Train model for 1000 episodes
+max_eps = 200001
+max_tsteps = 500
+upd_tsteps = 500
+aux_phase = 50
+
+rewards_ppg_kl, info_ppg_kl, ppg_KL = train_hockey_ppg(env_hockey, max_eps, max_tsteps, upd_tsteps, aux_phase, ppg_KL)
 
 
 # Save final model
-ppo_2.save_checkpoint(checkpoint_dir, "final")
+ppg_KL.save_checkpoint(checkpoint_dir, "final")
+
+np.save( "rewards_ppg_kl.npy", rewards_ppg_kl)
+np.stack("info_ppg_kl", info_ppg_kl)
---
+++ exp/run_client.py
@@ -0,0 +1,155 @@
+from __future__ import annotations
+
+import argparse
+import uuid
+
+import hockey.hockey_env as h_env
+import numpy as np
+
+
+from comprl.client import Agent, launch_client
+
+from PPG_GAE_KL_Beta import PPO, Memory
+
+
+class PPO_kimi(Agent):
+    def __init__(self, load_model = None):
+        super().__init__()
+
+        self.env = h_env.HockeyEnv(mode = h_env.Mode.NORMAL)
+        state_dim = self.env.observation_space.shape[0]
+        # action_dim = self.env.action_space.shape[0]
+
+        self.ppo = PPO(
+            state_dim,
+            4,
+            n_latent_var_actor = 128,
+            n_latent_var_critic = 128,
+            network_depth_actor = 2,
+            network_depth_critic = 2,
+            has_continuous_action_space = True,
+            action_std_init = 0.5,
+            lr = 0.0003,
+            betas = (0.9, 0.999),
+            gamma = 0.99,
+            K_epochs = 10,
+            eps_clip = 0.25,
+            c1 = 0.5,
+            c2 = 0.01,
+            beta_clone = 0.95
+        )
+
+        if load_model:
+            self.ppo.load_checkpoint(load_model)
+            print(f"Model was loaded: {load_model}")
+
+    def get_step(self, obv: list[float]) -> list[float]:
+
+        memory = None
+
+        state, _ = self.env.reset()
+        state = state.flatten()
+
+        action = self.ppo.policy_old.act(state, memory)
+        action = action.tolist()
+
+        return action
+
+    def on_start_game(self, game_id: int) -> None:
+        game_id = uuid.UUID(int = int.from_bytes(game_id, byteorder = "big"))
+        print(f"Game started (id: {game_id})")
+
+    def on_end_game(self, result: bool, stats: list[float]) -> None:
+        text_result = "won" if result else "lost"
+        print(
+            f"Game ended: {text_result} with my score: "
+            f"{stats[0]} against the opponent with score: {stats[1]}"
+        )
+
+
+class RandomAgent(Agent):
+    """A hockey agent that simply uses random actions."""
+
+    def get_step(self, observation: list[float]) -> list[float]:
+        return np.random.uniform(-1, 1, 4).tolist()
+
+    def on_start_game(self, game_id) -> None:
+        print("game started")
+
+    def on_end_game(self, result: bool, stats: list[float]) -> None:
+        text_result = "won" if result else "lost"
+        print(
+            f"game ended: {text_result} with my score: "
+            f"{stats[0]} against the opponent with score: {stats[1]}"
+        )
+
+
+class HockeyAgent(Agent):
+    """A hockey agent that can be weak or strong."""
+
+    def __init__(self, weak: bool) -> None:
+        super().__init__()
+
+        self.hockey_agent = h_env.BasicOpponent(weak=weak)
+
+    def get_step(self, observation: list[float]) -> list[float]:
+        # NOTE: If your agent is using discrete actions (0-7), you can use
+        # HockeyEnv.discrete_to_continous_action to convert the action:
+        #
+        # from hockey.hockey_env import HockeyEnv
+        # env = HockeyEnv()
+        # continuous_action = env.discrete_to_continous_action(discrete_action)
+
+        action = self.hockey_agent.act(observation).tolist()
+        return action
+
+    def on_start_game(self, game_id) -> None:
+        game_id = uuid.UUID(int=int.from_bytes(game_id))
+        print(f"Game started (id: {game_id})")
+
+    def on_end_game(self, result: bool, stats: list[float]) -> None:
+        text_result = "won" if result else "lost"
+        print(
+            f"Game ended: {text_result} with my score: "
+            f"{stats[0]} against the opponent with score: {stats[1]}"
+        )
+
+
+# Function to initialize the agent.  This function is used with `launch_client` below,
+# to lauch the client and connect to the server.
+def initialize_agent(agent_args: list[str]) -> Agent:
+    # Use argparse to parse the arguments given in `agent_args`.
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--agent",
+        type=str,
+        choices=["weak", "strong", "random", "my_agent"],
+        default="weak",
+        help="Which agent to use.",
+    )
+    parser.add_argument("--model", type=str, default=None)
+    args = parser.parse_args(agent_args)
+
+    # Initialize the agent based on the arguments.
+    agent: Agent
+    if args.agent == "weak":
+        agent = HockeyAgent(weak=True)
+    elif args.agent == "strong":
+        agent = HockeyAgent(weak=False)
+    elif args.agent == "random":
+        agent = RandomAgent()
+    elif args.agent == "my_agent":
+        agent = PPO_kimi(load_model = args.model)
+    else:
+        raise ValueError(f"Unknown agent: {args.agent}")
+
+    # And finally return the agent.
+    return agent
+
+
+def main() -> None:
+    launch_client(initialize_agent)
+
+
+if __name__ == "__main__":
+    main()
