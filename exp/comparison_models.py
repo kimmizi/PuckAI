c7ba19c2675a8a70f7630c804e003729026e7637
Kim Zierahn <kim@kappa-mm.de>
2025-02-25 13:05:05 +0100

    Update file paths for saving rewards in Pendulum environment

--- exp/comparison_models.py
+++ exp/comparison_models.py
@@ -458,28 +458,27 @@ if env == "Hockey":
 elif env == "Pendulum":
     print("Agent: ", ppg)
     rewards_ppg = train_pendulum_ppg(env_pendulum, max_eps, max_tsteps, upd_tsteps, ppg)
-    np.save( "./pendulum/rewards_ppg_pendulum.npy", rewards_ppg)
+    np.save( "../dat/pendulum/rewards_ppg_pendulum.npy", rewards_ppg)
     print("rewards_ppg_pendulum.npy saved")
 
     print("Agent: ", ppg_KL)
     rewards_ppg_kl = train_pendulum_ppg(env_pendulum, max_eps, max_tsteps, upd_tsteps, ppg_KL)
-    np.save( "./pendulum/rewards_ppg_kl_pendulum.npy", rewards_ppg_kl)
+    np.save( "../dat/pendulum/rewards_ppg_kl_pendulum.npy", rewards_ppg_kl)
     print("rewards_ppg_kl_pendulum.npy saved")
 
     print("Agent: ", ppg_beta)
     rewards_ppg_beta = train_pendulum_ppg(env_pendulum, max_eps, max_tsteps, upd_tsteps, ppg_beta)
-    np.save( "./pendulum/rewards_ppg_beta_pendulum.npy", rewards_ppg_beta)
+    np.save( "../dat/pendulum/rewards_ppg_beta_pendulum.npy", rewards_ppg_beta)
     print("rewards_ppg_beta_pendulum.npy saved")
 
     print("Agent: ", ppg_KL_beta)
     rewards_ppg_kl_beta = train_pendulum_ppg(env_pendulum, max_eps, max_tsteps, upd_tsteps, ppg_KL_beta)
-    np.save( "./pendulum/rewards_ppg_kl_beta_pendulum.npy", rewards_ppg_kl_beta)
+    np.save( "../dat/pendulum/rewards_ppg_kl_beta_pendulum.npy", rewards_ppg_kl_beta)
     print("rewards_ppg_kl_beta_pendulum.npy saved")
 
     print("Agent: ", ppo_vanilla)
     rewards_ppo = train_pendulum_ppo(env_pendulum, max_eps, max_tsteps, upd_tsteps, ppo_vanilla)
-    np.save( "./pendulum/rewards_ppo_pendulum.npy", rewards_ppo)
-    print("rewards_ppo_pendulum.npy saved")
+    np.save( "../dat/pendulum/rewards_ppo_pendulum.npy", rewards_ppo)
 
 elif env == "HalfCheetah":
     rewards_ppg = train_pendulum_ppg(env_halfcheetah, max_eps, max_tsteps, upd_tsteps, ppg)
